6fb0b8950c72687e7a4e263dffa145c1ad621c4a
shadowxmd2-cyber <shadowxmd2@gmail.com>
2025-12-15 20:07:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "bJVkAabQ#xJzVpB973DkVvYCNI3ry32ntqfe3dQmoaLnbHQWLmS0", // session id
+SESSION_ID: process.env.SESSION_ID || "b4IhzK6A#Ytl8sbsOG9wj6rNYh5WcNB78WtgNKlNAX5rrYJtmJVs", // session id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
